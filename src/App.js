0459ad02ee5545cf479289ff3b7bf37703bd11e7
YuLu19 <yuluyolo0619@gmail.com>
2020-12-04 13:43:58 +1300

    登录时显示Jonlist

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import AppMenu from './common/AppMenu';
+import JobList from './common/JobList'
+
+
+function App() {
+  return (
+    <div className="App">
+      <AppMenu />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/common/AppMenu.js
@@ -0,0 +1,75 @@
+import React, { useReducer } from 'react'
+import { Menu, Button } from 'semantic-ui-react'
+import Auth from '../authentication/Auth'
+import appMenuReducer from './AppMenuReducer'
+import JobList from './JobList'
+
+export default function AppMenu() {
+  const [state, dispatch] = useReducer(appMenuReducer, {
+    user: {},
+    authenticated: false,
+    authOpen: false
+  });
+
+  function handleOpenAuth() {
+    dispatch({type: 'AUTHENTICATING'});
+  }
+
+  function handleAuthClose() {
+    dispatch({type: 'UNAUTHENTICATED'});
+  }
+
+  function handleLogin(user) {
+    dispatch({type: 'AUTHENTICATED', payload: {user: user}});
+  }
+
+  function handleLogout() {
+    dispatch({type: 'UNAUTHENTICATED'});
+  }
+
+  return (
+    <div>
+      <Menu size='large'>
+        <Menu.Item
+            name='Heytour'
+        />
+
+        <Menu.Menu position='right'>
+          {!state.authenticated && 
+          <Menu.Item>
+            <Button primary onClick={handleOpenAuth}>
+              Login
+            </Button>
+          </Menu.Item>
+          }
+          
+          {state.authenticated &&
+            <Menu.Item name={state.user.firstName + ' ' + state.user.lastName} />
+          }
+
+          {state.authenticated &&
+            <Menu.Item>
+              <Button primary onClick={handleLogout}>
+                Logout
+              </Button>
+            </Menu.Item>
+          }
+        </Menu.Menu>
+      </Menu>
+      
+      {state.authenticated &&
+            <JobList />
+          }
+
+      {state.authOpen && 
+        <Auth
+          open={state.authOpen}
+          onClose={handleAuthClose}
+          onLogin={handleLogin}
+        />
+      }
+    </div>
+  )
+}
+
+
